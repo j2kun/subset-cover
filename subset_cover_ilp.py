6e610d5a3f5caa43f371fa42b9e007d373613628
Jeremy Kun <j2kun@users.noreply.github.com>
2020-10-13 22:09:17 -0700

    DRY for ilp model

--- subset_cover_ilp.py
+++ subset_cover_ilp.py
@@ -1,95 +1,15 @@
-from collections import defaultdict
-from dataclasses import dataclass
-from itertools import combinations
-from itertools import product
+from ilp_model import ChoiceSetMember
+from ilp_model import ChoiceSetMembers
+from ilp_model import IsHit
+from ilp_model import IsHits
+from ilp_model import make_hit_sets
 from subset_cover import SolveStatus
 from subset_cover import SubsetCover
 from subset_cover import SubsetCoverParameters
 from subset_cover import SubsetCoverSolution
 from time import time
-from typing import Any
-from typing import List
-from typing import Tuple
 
 from ortools.linear_solver import pywraplp
-
-
-@dataclass(frozen=True)
-class ChoiceSetMember:
-    '''Whether an element is chosen to be in a choice set.'''
-    element: int
-    choice_set_index: int
-    variable: Any
-
-
-@dataclass(frozen=True)
-class IsHit:
-    '''Whether a hit set is hit by a given choice set.'''
-    hit_set: Tuple[int]
-    choice_set_index: int
-    variable: Any
-
-
-class ChoiceSetMembers:
-    '''A class that constructs ChoiceSetMember instances and the associated lookups.'''
-    def __init__(self, solver, elements, choice_set_indices):
-        self.memberships = [
-            ChoiceSetMember(element=element,
-                            choice_set_index=choice_set_index,
-                            variable=solver.IntVar(
-                                0, 1,
-                                f"Member_({choice_set_index},{element})"))
-            for (element,
-                 choice_set_index) in product(elements, choice_set_indices)
-        ]
-
-        self.memberships_lookup = dict(
-            ((mem.choice_set_index, mem.element), mem)
-            for mem in self.memberships)
-
-        self.memberships_by_choice_set = defaultdict(set)
-        for membership in self.memberships:
-            self.memberships_by_choice_set[membership.choice_set_index].add(
-                membership)
-
-    def for_choice_set_index_and_element(self, choice_set_index, element):
-        return self.memberships_lookup[(choice_set_index, element)]
-
-    def for_choice_set_index(self, choice_set_index):
-        return self.memberships_by_choice_set[choice_set_index]
-
-
-def make_hit_sets(elements, hit_set_size):
-    return [
-        tuple(sorted(elts)) for elts in combinations(elements, hit_set_size)
-    ]
-
-
-class IsHits:
-    '''A class that defines IsHit variables for each
-    hit set and choice set pair.
-    '''
-    def __init__(self, solver, hit_sets, choice_set_indices):
-        self.is_hit_variables = [
-            IsHit(hit_set=hit_set,
-                  choice_set_index=choice_set_index,
-                  variable=solver.IntVar(
-                      0, 1, f"IsHit_({hit_set},{choice_set_index})")) for
-            hit_set, choice_set_index in product(hit_sets, choice_set_indices)
-        ]
-
-        self.lookup = {(x.hit_set, x.choice_set_index): x
-                       for x in self.is_hit_variables}
-
-        self.by_hit_set = defaultdict(set)
-        for is_hit in self.is_hit_variables:
-            self.by_hit_set[is_hit.hit_set].add(is_hit)
-
-    def all(self):
-        return self.is_hit_variables
-
-    def for_hit_set(self, hit_set):
-        return self.by_hit_set[hit_set]
 
 
 class SubsetCoverILP(SubsetCover):
@@ -172,8 +92,8 @@ class SubsetCoverILP(SubsetCover):
 if __name__ == "__main__":
     result = SubsetCoverILP().solve(
         SubsetCoverParameters(num_elements=7,
-                              hit_set_size=3,
-                              choice_set_size=2,
+                              choice_set_size=3,
+                              hit_set_size=2,
                               num_choice_sets=4))
 
     print(result)
